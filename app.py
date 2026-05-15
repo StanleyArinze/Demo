72450e790cf842c5c5ac8261f951fef4543c7a3a
StanleyArinze <Sirarinzeugwuoke@gmail.com>
2026-05-15 14:27:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def load_data():
     """
     Load cleaned air quality dataset from GitHub repository.
     """
-    file_path = "Task2_Cleaned_Data.csv"
+    file_path = "beijing_air_quality_cleaned.csv"
 
     if not os.path.exists(file_path):
         return pd.DataFrame()
