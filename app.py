73837d1baa30642fefc4194b8c6147b028db7c84
StanleyArinze <Sirarinzeugwuoke@gmail.com>
2026-05-16 10:48:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -419,9 +419,9 @@ visual_tab1, visual_tab2, visual_tab3, visual_tab4, visual_tab5, visual_tab6, vi
             # TAB 1: DISTRIBUTION
             # --------------------------------------------------------
 
-        with visual_tab1:
+            with visual_tab1:
 
-                color_column = "category" if "category" in df.columns else None
+               color_column = "category" if "category" in df.columns else None
 
                 fig = px.histogram(
                     df,
